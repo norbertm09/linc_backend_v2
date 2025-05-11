0a55640683011a030e0a796ee7d5e28974126290
norbertm09 <malu.n@calculus-system.cd>
2025-05-12 01:39:22 +0200

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from routers import users
 from routers import auth, wallets, transfers, crypto, savings, insurance, scoring, credits, admin, notifications
 app = FastAPI(title="Linc API v1.8")
 
@@ -12,6 +13,7 @@ app.include_router(scoring.router)
 app.include_router(credits.router)
 app.include_router(admin.router)
 app.include_router(notifications.router)
+app.include_router(users.router)
 
 @app.get("/")
 def root():
