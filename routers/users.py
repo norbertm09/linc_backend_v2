8e3fb6d580a10f01663e108f06cfa33c8e7f91a1
norbertm09 <malu.n@calculus-system.cd>
2025-05-12 02:01:14 +0200

    users.py

--- routers/users.py
+++ routers/users.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from database import SessionLocal
 from sqlalchemy import text
-
+from database import SessionLocal
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -16,4 +15,4 @@ def get_db():
 @router.get("/")
 def list_users(db: Session = Depends(get_db)):
     result = db.execute(text("SELECT id, email, full_name FROM users")).fetchall()
-       return [dict(row) for row in result]
+    return [dict(row) for row in result]
